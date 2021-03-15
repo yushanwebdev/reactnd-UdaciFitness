02970668229a58b7b52640035b149391ad736d9a
yushan <yushan@intervest.lk>
2021-03-15 20:38:50 +0530

    feat: Bike icon added to the page using getMetricMetaInfo

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
 
 export default class AddEntry extends Component { 
     render() {
         return(
             <View>
-                <Text>Add Entry</Text>
+                {getMetricMetaInfo('bike').getIcon()}
             </View>
         )
     }
