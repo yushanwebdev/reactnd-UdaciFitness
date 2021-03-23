7064428b334a4521fb5d56a9a8ef1948a50f958f
yushan <yushan@intervest.lk>
2021-03-23 19:43:50 +0530

    feat: Add state to the <AddEntry />

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,14 @@ import { View } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
 
 export default class AddEntry extends Component { 
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    }
+
     render() {
         return(
             <View>
