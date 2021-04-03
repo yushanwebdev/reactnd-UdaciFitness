74bdedacf257105f6d1889ae1a7a7c17ed1afb77
yushan <yushan@intervest.lk>
2021-04-03 15:11:49 +0530

    feat: Create a another UI to show when user already logged their information of the day.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from '../components/UdaciSlider';
 import UdaciSteppers from '../components/UdaciSteppers';
 import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton';
 
 function SubmitBtn({ onPress }) {
     return (
@@ -74,8 +76,32 @@ export default class AddEntry extends Component {
         // Clear local notification
     }
 
+    reset = () => {
+        const key = timeToString();
+
+        // Update Redux
+
+        // Route to Home
+
+        // Update "DB"
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo();
+
+        if(this.props.alreadyLogged) {
+            return(
+                <View>
+                    <Ionicons 
+                        name="ios-happy-outline"
+                        size={100} />
+                    <Text>You already logged your information today.</Text>
+                    <TextButton onPress={this.reset}>
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
 
         return (
             <View>
