332c184035e7c4a6b0bac33c8f4ed1bb6df728ba
yushan <yushan@intervest.lk>
2021-04-03 13:19:16 +0530

    feat: Develop the <UdaciSlider /> component's slider functionality

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Slider from '@react-native-community/slider';
 
-export default function UdaciSlider() {
+export default function UdaciSlider({ max, unit, step, value, onChange }) {
     return(
         <View>
-            <Text>UdaciSlider</Text>
+            <Slider 
+                step={step}
+                value={value}
+                maximumValue={max}
+                minimumValue={0}
+                onValueChange={onChange} />
+            <Text>{value}</Text>
+            <Text>{unit}</Text>
         </View>
     )
 }
