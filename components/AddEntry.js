856e50a0735c8ac78d1a1eac28ff42f19753d62a
yushan <yushan@intervest.lk>
2021-04-04 11:57:27 +0530

    feat: submit & reset methods in <AddEntry /> connected to AsyncStorage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from '../components/UdaciSteppers';
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
 
 function SubmitBtn({ onPress }) {
     return (
@@ -71,7 +72,7 @@ export default class AddEntry extends Component {
 
         // Navigate to Home
 
-        // Save to 'DB'
+        submitEntry({ key, entry });
 
         // Clear local notification
     }
@@ -83,7 +84,7 @@ export default class AddEntry extends Component {
 
         // Route to Home
 
-        // Update "DB"
+        removeEntry(key);
     }
 
     render() {
