909c0a60fb069478e4e9d577f9a9f9b2c146ece1
yushan <yushan@intervest.lk>
2021-04-07 21:39:39 +0530

    test: align-items: "center"

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'flex-start'
+    alignItems: 'center'
   },
   box: {
     height: 50,
