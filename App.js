21de2382d2ddcecb976461d54c30b06ada1741ef
yushan <yushan@intervest.lk>
2021-04-07 21:35:40 +0530

    test: justify-content: "space-between"

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end'
+    justifyContent: 'space-between'
   },
   box: {
     height: 50,
