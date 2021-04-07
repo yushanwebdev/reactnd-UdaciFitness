9486496e56257589a1092938dd164362fce0cd34
yushan <yushan@intervest.lk>
2021-04-07 21:34:47 +0530

    test: justify-content: flex-end

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'flex-end'
   },
   box: {
     height: 50,
