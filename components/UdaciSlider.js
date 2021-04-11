55306a762a06e376cc1748f0aa76d9f4a9a9010b
yushan <yushan@intervest.lk>
2021-04-11 19:18:11 +0530

    feat: Style the <UdaciSlider />

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Slider from '@react-native-community/slider';
+import { gray } from '../utils/colors';
 
 export default function UdaciSlider({ max, unit, step, value, onChange }) {
-    return(
-        <View>
-            <Slider 
+    return (
+        <View style={styles.row}>
+            <Slider
                 step={step}
                 value={value}
                 maximumValue={max}
                 minimumValue={0}
-                onValueChange={onChange} />
-            <Text>{value}</Text>
-            <Text>{unit}</Text>
+                onValueChange={onChange}
+                style={{flex: 1}} />
+            <View style={styles.metricCounter}>
+                <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
