7667d8737aecc35da35fd6896cab438dc3572da2
yushan <yushan@intervest.lk>
2021-04-12 12:47:50 +0530

    feat: Develop <History /> to view all calendar dates results

--- components/History.js
+++ components/History.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { addEntry, receiveEntries } from '../actions';
+import { fetchCalendarResults } from '../utils/api';
+import { getDailyReminderValue, timeToString } from '../utils/helpers';
 
 class History extends Component {
+    componentDidMount() {
+        const { dispatch } = this.props;
+
+        fetchCalendarResults()
+            .then((entries) => dispatch(receiveEntries(entries)))
+            .then(({ entries }) => {
+                if(!entries[timeToString()]) {
+                    dispatch(addEntry({
+                        [timeToString()]: getDailyReminderValue()
+                    }))
+                }
+            })
+    }
+
     render() {
         return(
             <View>
-                <Text>History</Text>
+                <Text>{JSON.stringify(this.props)}</Text>
             </View>
         )
     }
 }
 
-export default History;
+function mapStateToProps(entries) {
+    return {    
+        entries
+    }
+}
+
+export default connect(mapStateToProps)(History);
