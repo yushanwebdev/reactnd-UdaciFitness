482468d3e71a92ca9c5bd42ce8322502357a39a3
yushan <yushan@intervest.lk>
2021-04-12 12:41:36 +0530

    fix: AsyncStorage is undefined issue

--- utils/_calendar.js
+++ utils/_calendar.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 import { getMetricMetaInfo, timeToString } from './helpers'
 
 export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar'
