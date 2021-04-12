5898f93356b43b920c732daea6b0c79031fb4348
yushan <yushan@intervest.lk>
2021-04-12 20:08:41 +0530

    feat: Style the <History />

--- components/History.js
+++ components/History.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { connect } from 'react-redux';
-import { Agenda as UdaciFitnessCalendar } from 'react-native-calendars'
+import { Agenda as UdaciFitnessCalendar } from 'react-native-calendars';
 import { addEntry, receiveEntries } from '../actions';
 import { fetchCalendarResults } from '../utils/api';
 import { getDailyReminderValue, timeToString } from '../utils/helpers';
+import { white } from '../utils/colors';
+import DateHeader from './DateHeader';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 class History extends Component {
     componentDidMount() {
@@ -21,18 +24,27 @@ class History extends Component {
             })
     }
 
-    renderItem = ({ today, ...metrics }, formattedDate, key) => (
-        <View>
+    renderItem = ({ today, ...metrics }, firstItemInDay) => (
+        <View style={styles.item}>
             {today
-                ? <Text>{JSON.stringify(today)}</Text>
-                : <Text>{JSON.stringify(metrics)}</Text>
+                ? <View>
+                    <Text style={styles.noDataText}>
+                        {today}
+                    </Text>
+                </View>
+                : <TouchableOpacity onPress={() => console.log('Pressed!')}>
+                    <Text>{JSON.stringify(metrics)}</Text>
+                </TouchableOpacity>
             }
         </View>
     )
 
     renderEmptyDate = (formattedDate) => (
-        <View>
-            <Text>No Data for this day</Text>
+        <View style={styles.item}>
+            
+            <Text style={styles.noDataText}>
+                You didn't log any data on this day.
+            </Text>
         </View>
     )
 
@@ -40,12 +52,36 @@ class History extends Component {
         const { entries } = this.props;
         return(
             <UdaciFitnessCalendar 
-                items = {entries}
-                renderItem = {this.renderItem}
-                renderEmptyDate = {this.renderEmptyDate} />
+                items={entries}
+                renderItem={this.renderItem}
+                renderEmptyDate={this.renderEmptyDate} />
         )
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        }
+    },
+    noDataText: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+})
 
 function mapStateToProps(entries) {
     return {    
