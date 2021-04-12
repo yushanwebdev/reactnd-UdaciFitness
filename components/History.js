b6c3e525ee3f74140126e44327b55bdb22013745
yushan <yushan@intervest.lk>
2021-04-12 12:29:27 +0530

    feat: <History /> skeleton created.

---
+++ components/History.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class History extends Component {
+    render() {
+        return(
+            <View>
+                <Text>History</Text>
+            </View>
+        )
+    }
+}
+
+export default History;
