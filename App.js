3d321df4fb2101ecc77c1e53dd0ed8966538e638
yushan <yushan@intervest.lk>
2021-04-15 21:27:01 +0530

    test: Develop a basic bottom tab navigator

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function Home() {
+  return(
+    <View>
+      <Text>HOME</Text>
+    </View>
+  )
+}
+
+function Dashboard() {
+  return(
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <Provider store={createStore(reducer)}> 
-      <View style={{flex: 1}}>
-        <View style={{height: 20}} />
-        <History />
-      </View>
-    </Provider>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Dashboard" component={Dashboard} />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
