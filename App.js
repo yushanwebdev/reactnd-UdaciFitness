9ed6728a0797e11318f9f4d8cd6bf65aa4ad0ad0
yushan <yushan@intervest.lk>
2021-04-15 20:11:14 +0530

    test: Style the <App /> using "styled-components/native"

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
+import styled from 'styled-components/native';
+
+const CenterView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #333;
+`;
+
+const WelcomeText = styled.Text`
+  color: white;
+  font-size: 20px;
+`
+
+const WelcomeBtn = styled.TouchableOpacity`
+  width: 100px;
+  height: 50px;
+  background: red;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+`
 
 export default function App() {
   return (
-    <Provider store={createStore(reducer)}> 
-      <View style={{flex: 1}}>
-        <View style={{height: 20}} />
-        <History />
-      </View>
-    </Provider>
+    <CenterView>
+      <WelcomeText>Hello</WelcomeText>
+      <WelcomeBtn onPress={() => alert('Hello')}>
+        <WelcomeText>Push Me</WelcomeText>
+      </WelcomeBtn>
+    </CenterView>
   );
 }
