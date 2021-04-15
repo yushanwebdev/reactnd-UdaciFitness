33355002a706f36a6dfda9445ac24f1911f3bb96
yushan <yushan@intervest.lk>
2021-04-15 21:31:35 +0530

    test: Add an icon to bottom tab navigator

--- App.js
+++ App.js
@@ -7,9 +7,10 @@ import reducer from './reducers';
 import History from './components/History';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function Home() {
-  return(
+  return (
     <View>
       <Text>HOME</Text>
     </View>
@@ -17,7 +18,7 @@ function Home() {
 }
 
 function Dashboard() {
-  return(
+  return (
     <View>
       <Text>Dashboard</Text>
     </View>
@@ -29,10 +30,26 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Dashboard" component={Dashboard} />
-    </Tab.Navigator>
-  </NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Dashboard') {
+              iconName = focused ? 'ios-list-box' : 'ios-list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Dashboard" component={Dashboard} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
