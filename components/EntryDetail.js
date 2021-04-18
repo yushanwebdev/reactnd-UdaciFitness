443c89c6a78a2e2bffe92cf74b722b132777b209
yushan <yushan@intervest.lk>
2021-04-18 13:46:29 +0530

    feat: Add reset UI & functionality to <EntryDetail />

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -3,13 +3,33 @@ import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { white } from '../utils/colors';
 import MetricCard from './MetricCard';
+import { addEntry } from '../actions';
+import { removeEntry } from '../utils/api';
+import { timeToString, getDailyReminderValue } from '../utils/helpers';
+import TextButton from './TextButton';
 
 class EntryDetail extends Component {
+  reset = () => {
+    const { remove, goBack, entryId } = this.props;
+
+    remove();
+    goBack();
+    removeEntry(entryId);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const metrics = nextProps.metrics;
+    return metrics.length && !metrics.today
+  }
+
   render() {
     const { metrics } = this.props;
     return (
       <View style={styles.container}>
         <MetricCard metrics={metrics[0]} />
+        <TextButton onPress={this.reset} style={{margin: 20}}>
+          RESET
+        </TextButton>
       </View>
     )
   }
@@ -31,4 +51,17 @@ function mapStateToProps(state, { route }) {
   }
 }
 
-export default connect(mapStateToProps)(EntryDetail);
+function mapDispatchToProps(dispatch, { navigation, route }) {
+  const { entryId } = route.params;
+
+  return {
+    remove: () => dispatch(addEntry({
+      [entryId]: timeToString() === entryId
+        ? getDailyReminderValue()
+        : new Array()
+    })),
+    goBack: () => navigation.goBack()
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail);
