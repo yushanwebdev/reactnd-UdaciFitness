23fce6ad3e2879d427f813c0ba550d89779a6920
yushan <yushan@intervest.lk>
2021-04-18 15:06:44 +0530

    test: Implement the Drawer navigation to <App />

--- App.js
+++ App.js
@@ -1,54 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function Home({ navigation }) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>HOME</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
-        <Text>To Dashboard</Text>
+      <TouchableOpacity onPress={() => navigation.openDrawer()}>
+        <Text>Open Drawer</Text>
       </TouchableOpacity>
     </View>
   )
 }
 
-function Dashboard() {
+function Dashboard({ navigation }) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Dashboard</Text>
+      <TouchableOpacity onPress={() => navigation.openDrawer()}>
+        <Text>Open Drawer</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
+      <Drawer.Navigator>
+        <Drawer.Screen 
           name="Home" 
           component={Home} />
-        <Stack.Screen 
+        <Drawer.Screen 
           name="Dashboard" 
-          component={Dashboard} 
-          options={{ 
-            title: 'Overview',
-            headerTintColor: 'red',
-            headerStyle: {
-              backgroundColor: 'green'
-            }
-          }} />
-      </Stack.Navigator>
+          component={Dashboard} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
