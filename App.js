c4d8b7b8db55c6e90c49268636c87cbcefa83ef3
yushan <yushan@intervest.lk>
2021-04-22 12:36:55 +0530

    test: Develop the <App /> with image picking feature

--- App.js
+++ App.js
@@ -3,14 +3,49 @@ import {
   Text,
   View,
   StyleSheet,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import ImageEditor from '@react-native-community/image-editor';
 
 
 export default class App extends React.Component {
+  state = {
+    image: null,
+  }
+
+  pickImage = () => {
+    ImagePicker.launchImageLibraryAsync({
+      allowEditing: true,
+      aspect: [2,1],
+    }).then((result) => {
+      if(result.cancelled) {
+        return
+      }
+
+      ImageEditor.cropImage(result.uri, {
+        offset: { x: 0, y: 0 },
+        size: { width: result.width, height: result.height },
+        displaySize: { width: 200, height: 100 },
+        resizeMode: 'contain',
+      },
+      (uri) => this.setState(() => ({ image: uri })),
+      () => console.log('Error'))
+    })
+  }
+
   render() {
+    const { image } = this.state;
     return(
       <View style={styles.container}>
+        <TouchableOpacity onPress={this.pickImage}>
+          <Text>Open Camera Roll</Text>
+        </TouchableOpacity>
 
+        {image && (
+          <Image style={styles.img} source={{uri: image}} />
+        )}
       </View>
     )
   }
